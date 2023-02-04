edf2f1bb7525d04c4f4b7a306bb7390996717710
Matthew Szklany <mszklan1@cs.binghamton.edu>
2023-02-04 15:26:15 -0500

    starting parsing

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import pandas as pd
+import numpy as np
+
 def main():
-  print("sup gamers")
+  #print("sup gamers")
+  df = pd.read_excel('data/show_req_sample.xlsx')
+  print(df)
  
 if __name__ == "__main__":
   main()
