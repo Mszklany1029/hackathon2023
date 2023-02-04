6e4b52bb474272d5296e7db0e88083e29d4d8f58
BuildTools <unconfigured@null.spigotmc.org>
2023-02-04 15:09:19 -0500

    fixed missed renaming

--- person.py
+++ person.py
@@ -3,5 +3,5 @@ class Person:
         self.name = name
         self.hours = hours
         self.student = student # 0 = community member, 1-4 = year in school, 5 = eboard
-        self.preferred_availability = preferred_time # list in preference order
-        self.other_availability = other_time # list in no order
+        self.preferred_time = preferred_time # list in preference order
+        self.other_time = other_time # list in no order
